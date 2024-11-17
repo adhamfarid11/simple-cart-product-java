cc93dc5e8bf3c9d8c2b6a5b19d9d25c739c00fd5
Adham Muhammad <adhamfarid1110@gmail.com>
2024-11-17 13:44:26 +0700

    init: project

---
+++ src/test/java/com/wide_tech_interview/cart_product/CartProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wide_tech_interview.cart_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
